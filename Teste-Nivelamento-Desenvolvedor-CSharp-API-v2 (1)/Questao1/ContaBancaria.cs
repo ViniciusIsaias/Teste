7c1278fb36a5049be4b33cddf376e742dda61a0e
vinii <vinii@DESKTOP-NCK1K27>
2023-09-21 11:40:37 -0300

    Commit 1

---
+++ Teste-Nivelamento-Desenvolvedor-CSharp-API-v2 (1)/Questao1/ContaBancaria.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+namespace Questao1
+{
+    public class ContaBancaria {
+
+        private double TaxaSaque = 3.50;
+
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double deposito)
+        {
+            Numero = numero;
+            Titular = titular;
+            Saldo = deposito;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= valor;
+            Saldo -= TaxaSaque;
+        }
+
+        public int Numero { get; set; }
+
+        public string Titular { get; set; }
+
+        public double Saldo { get; set; }
+    }
+}
